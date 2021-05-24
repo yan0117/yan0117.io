23ca6a1ea86a1b6fe8a4ea2dd18b73d9ebb3ba9b
yan0117 <84706376+yan0117@users.noreply.github.com>
2021-05-24 21:01:00 +0800

    Add files via upload

---
+++ 14-2/main.cpp
@@ -0,0 +1,41 @@
+#include <GL/glut.h>
+
+void display()
+
+{
+
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+    glutWireSphere(0.3,30,30);
+
+    glutSwapBuffers();
+
+}
+
+void timer(int t)
+
+{
+
+    glClearColor(1,0,0,0);//清除背景的顏色 改紅色
+
+    display();
+
+}
+
+int main(int argc,char **argv)
+
+{
+
+    glutInit(&argc,argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE| GLUT_DEPTH);
+
+    glutCreateWindow("week14");
+
+    glutDisplayFunc(display);
+
+    glutTimerFunc(4000,timer,0);//等多久 叫誰 參數
+
+    glutMainLoop();
+
+}
